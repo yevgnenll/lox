4a6356d73a8fce3d1d0d8be0eb6e5413bc70fd51
yevgnenll <yevgnenll@gmail.com>
2024-06-22 21:07:28 +0900

    feat: token 구현

--- app/src/main/java/me/yevgnenll/lox/Token.java
+++ app/src/main/java/me/yevgnenll/lox/Token.java
@@ -1,4 +1,19 @@
 package me.yevgnenll.lox;
 
 public class Token {
+    final TokenType type;
+    final String lexeme;
+    final Object literal;
+    final int line;
+
+    Token(TokenType tokenType, String lexeme, Object literal, int line) {
+        this.type = tokenType;
+        this.lexeme = lexeme;
+        this.literal = literal;
+        this.line = line;
+    }
+
+    public String toString() {
+        return type + " " + lexeme + " " + literal;
+    }
 }
