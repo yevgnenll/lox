6fe740f1fe173fc39df21f00c6ebc582f9ba6b3c
yevgnenll <yevgnenll@gmail.com>
2024-06-22 20:35:54 +0900

    feat: interface 추가

---
+++ app/src/main/java/me/yevgnenll/lox/Scanner.java
@@ -0,0 +1,12 @@
+package me.yevgnenll.lox;
+
+import java.util.List;
+
+public class Scanner {
+    public Scanner(String source) {
+    }
+
+    public List<Token> scanTokens() {
+        return null;
+    }
+}
---
+++ app/src/main/java/me/yevgnenll/lox/Token.java
@@ -0,0 +1,4 @@
+package me.yevgnenll.lox;
+
+public class Token {
+}
