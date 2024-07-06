7def31e43320222d6759144acd647013edf8885c
yevgnenll <yevgnenll@gmail.com>
2024-07-06 19:45:55 +0900

    chore: 불필요한 코드 제거

--- app/src/main/java/me/yevgnenll/lox/AstPrinter.java
+++ app/src/main/java/me/yevgnenll/lox/AstPrinter.java
@@ -2,20 +2,6 @@ package me.yevgnenll.lox;
 
 public class AstPrinter implements Expr.Visitor {
 
-    public static void main(String[] args) {
-        Expr expr = new Expr.Binary(
-                new Expr.Unary(
-                        new Token(TokenType.MINUS, "-", null, 1),
-                        new Expr.Literal(123)),
-                    new Token(TokenType.STAR, "*", null, 1),
-                    new Expr.Grouping(
-                            new Expr.Literal(46.67)
-                    )
-                );
-
-        System.out.println(new AstPrinter().print(expr));
-    }
-
     String print(Expr expr) {
         return (String) expr.accept(this);
     }
