d80169a6b5f5d107251172f60f1f0aeb0579f824
yevgnenll <yevgnenll@gmail.com>
2024-07-13 12:00:52 +0900

    feat: interpretor

---
+++ app/src/main/java/me/yevgnenll/lox/Interpretor.java
@@ -0,0 +1,155 @@
+package me.yevgnenll.lox;
+
+public class Interpretor implements Expr.Visitor<Object> {
+
+    @Override
+    public Object visitLiteralExpr(Expr.Literal expr) {
+        return expr.value;
+    }
+
+    @Override
+    public Object visitGroupingExpr(Expr.Grouping expr) {
+        return evaluate(expr.expression);
+    }
+
+    private Object evaluate(Expr expr) {
+        return expr.accept(this);
+    }
+
+    @Override
+    public Object visitBinaryExpr(Expr.Binary expr) {
+        Object left = evaluate(expr.left);
+        Object right = evaluate(expr.right);
+
+        switch (expr.operator.type) {
+            case GREATER -> {
+                checkNumberOperands(expr.operator, left, right);
+                return (double) left > (double) right;
+            }
+            case GREATER_EQUAL -> {
+                checkNumberOperands(expr.operator, left, right);
+                return (double) left >= (double) right;
+            }
+            case LESS -> {
+                checkNumberOperands(expr.operator, left, right);
+                return (double) left < (double) right;
+            }
+            case LESS_EQUAL -> {
+                checkNumberOperands(expr.operator, left, right);
+                return (double) left <= (double) right;
+            }
+            case MINUS -> {
+                checkNumberOperands(expr.operator, left, right);
+                return (double) left - (double) right;
+            }
+            case PLUS -> {
+                if (left instanceof Double && right instanceof Double) {
+                    return (double) left + (double) right;
+                }
+
+                if (left instanceof String && right instanceof String) {
+                    return (String) left + (String) right;
+                }
+                throw new RuntimeError (expr.operator,
+                        "Operands must be two numbers or two strings.");
+            }
+            case SLASH -> {
+                checkNumberOperands(expr.operator, left, right);
+                return (double) left / (double) right;
+            }
+            case STAR -> {
+                checkNumberOperands(expr.operator, left, right);
+                return (double) left * (double) right;
+            }
+            case EQUAL_EQUAL -> {
+                return isEqual(left, right);
+            }
+            case BANG_EQUAL -> {
+                return !isEqual(left, right);
+            }
+        }
+        // unreachable
+        return null;
+    }
+
+    private boolean isEqual(Object a, Object b) {
+        if (a == null && b == null) return true;
+        if (a == null) return false;
+        return a.equals(b);
+    }
+
+    @Override
+    public Object visitUnaryExpr(Expr.Unary expr) {
+        Object right = evaluate(expr.right);
+
+        switch (expr.operator.type) {
+            case MINUS -> {
+                checkNumberOperand(expr.operator, right);
+                return - (double) right;
+            }
+
+            case BANG -> {
+                return !isTruthy(right);
+            }
+        }
+        // unreachable
+        return null;
+    }
+
+    private boolean isTruthy(Object object) {
+        if (object == null) return false;
+        if (object instanceof Boolean) {
+            return (boolean) object;
+        }
+        return true;
+    }
+    private void checkNumberOperand (Token operator, Object operand) {
+        if (operand instanceof Double) return;
+        throw new RuntimeError(operator, "Operand must be a number.");
+    }
+
+    private void checkNumberOperands (Token operator, Object left, Object right) {
+        if (left instanceof Double && right instanceof Double) return;
+        throw new RuntimeError(operator, "Operands must be numbers.");
+    }
+
+    @Override
+    public Object visitAssignExpr(Expr.Assign expr) {
+        return null;
+    }
+
+    @Override
+    public Object visitCallExpr(Expr.Call expr) {
+        return null;
+    }
+
+    @Override
+    public Object visitGetExpr(Expr.Get expr) {
+        return null;
+    }
+
+    @Override
+    public Object visitLogicalExpr(Expr.Logical expr) {
+        return null;
+    }
+
+    @Override
+    public Object visitSetExpr(Expr.Set expr) {
+        return null;
+    }
+
+    @Override
+    public Object visitSuperExpr(Expr.Super expr) {
+        return null;
+    }
+
+    @Override
+    public Object visitThisExpr(Expr.This expr) {
+        return null;
+    }
+
+    @Override
+    public Object visitVariableExpr(Expr.Variable expr) {
+        return null;
+    }
+}
---
+++ app/src/main/java/me/yevgnenll/lox/RuntimeError.java
@@ -0,0 +1,12 @@
+package me.yevgnenll.lox;
+
+public class RuntimeError extends RuntimeException {
+
+    final Token token;
+
+    public RuntimeError(Token token, String message) {
+        super(message);
+        this.token = token;
+    }
+}
+
