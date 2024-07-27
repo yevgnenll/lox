a4cac09fe4a05213be3fab545fae4b81849c601f
yevgnenll <yevgnenll@gmail.com>
2024-07-27 19:39:24 +0900

    chore: stmt 컨벤션 반영

--- app/src/main/java/me/yevgnenll/lox/Stmt.java
+++ app/src/main/java/me/yevgnenll/lox/Stmt.java
@@ -2,37 +2,37 @@ package me.yevgnenll.lox;
 
 import java.util.List;
 
-abstract class Stmt{
+abstract class Stmt {
     interface Visitor<R> {
-    R visitExpressionStmt(Expression stmt);
-    R visitPrintStmt(Print stmt);
+        R visitExpressionStmt(Expression stmt);
+        R visitPrintStmt(Print stmt);
     }
-   static class Expression extends Stmt{
+   static class Expression extends Stmt {
     Expression(Expr expression) {
     // constructor
       this.expression = expression;
     }
 
 
-    @Override
-    <R> R accept(Visitor<R> visitor) {
+     @Override
+     <R> R accept(Visitor<R> visitor) {
         return visitor.visitExpressionStmt(this);
      }
- // fields
+     // fields
      final Expr expression;
    }
-   static class Print extends Stmt{
+   static class Print extends Stmt {
     Print(Expr expression) {
     // constructor
       this.expression = expression;
     }
 
 
-    @Override
-    <R> R accept(Visitor<R> visitor) {
+     @Override
+     <R> R accept(Visitor<R> visitor) {
         return visitor.visitPrintStmt(this);
      }
- // fields
+     // fields
      final Expr expression;
    }
 
